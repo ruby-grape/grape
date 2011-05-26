b7deeb066bb7e722f2ff59e19b8b1127153b4a57
Michael Bleigh <michael@intridea.com>
2011-05-26 18:41:45 -0500

    Updating OAuth2 authorization mode a bit, first round.

--- lib/grape/middleware/auth/oauth2.rb
+++ lib/grape/middleware/auth/oauth2.rb
@@ -3,16 +3,31 @@ module Grape::Middleware::Auth
     def default_options
       {
         :token_class => 'AccessToken',
-        :realm => 'OAuth API'
+        :realm => 'OAuth API',
+        :parameter => %w(bearer_token oauth_token),
+        :header => [/Bearer (.*)/i, /OAuth (.*)/i]
       }
     end
     
     def before
-      if request['oauth_token']
-        verify_token(request['oauth_token'])
-      elsif env['Authorization'] && t = parse_authorization_header
-        verify_token(t)
+      verify_token(token_parameter || token_header)
+    end
+
+    def token_parameter
+      Array(options[:parameter]).each do |p|
+        return request[p] if request[p]
       end
+      nil
+    end
+
+    def token_header
+      return false unless env['Authorization']
+      Array(options[:header]).each do |regexp|
+        if env['Authorization'] =~ regexp
+          return $1
+        end
+      end
+      nil
     end
     
     def token_class
@@ -21,7 +36,7 @@ module Grape::Middleware::Auth
     
     def verify_token(token)
       if token = token_class.verify(token)
-        if token.expired?
+        if token.respond_to?(:expired?) && token.expired?
           error_out(401, 'expired_token')
         else
           if token.permission_for?(env)
@@ -43,7 +58,7 @@ module Grape::Middleware::Auth
     
     def error_out(status, error)
       throw :error, {
-        :message => 'The token provided has expired.',
+        :message => error,
         :status => status,
         :headers => {
           'WWW-Authenticate' => "OAuth realm='#{options[:realm]}', error='#{error}'"
