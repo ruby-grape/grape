7bbdc897e1a0cd2668fb506aa9ba4b6e5b61b88b
Michael Bleigh <michael@intridea.com>
2011-11-10 11:07:58 +0800

    Adds exposures that are block-driven.

--- lib/grape/entity.rb
+++ lib/grape/entity.rb
@@ -10,10 +10,11 @@ module Grape
   #   module API
   #     module Entities
   #       class User < Grape::Endpoint
-  #         expose :name, :screen_name, :location
+  #         expose :first_name, :last_name, :screen_name, :location
   #         expose :latest_status, :using => API::Status, :as => :status, :unless => {:collection => true}
   #         expose :email, :if => {:type => :full}
   #         expose :new_attribute, :if => {:version => 'v2'}
+  #         expose(:name){|model,options| [model.first_name, model.last_name].join(' ')}
   #       end
   #     end
   #   end
@@ -57,12 +58,20 @@ module Grape
     #   automatically be transformed into a representation that will receive the same
     #   options as the parent entity when called. Note that arrays are fine here and
     #   will automatically be detected and handled appropriately.
-    def self.expose(*args)
+    # @option options :proc If you pass a Proc into this option, it will
+    #   be used directly to determine the value for that attribute. It
+    #   will be called with the represented object as well as the
+    #   runtime options that were passed in. You can also just supply a
+    #   block to the expose call to achieve the same effect.
+    def self.expose(*args, &block)
       options = args.last.is_a?(Hash) ? args.pop : {}
 
-      if options[:as] && args.size > 1
-        raise ArgumentError, "You may not use the :as option on multi-attribute exposures."
+      if args.size > 1
+        raise ArgumentError, "You may not use the :as option on multi-attribute exposures." if options[:as]
+        raise ArgumentError, "You may not use block-setting on multi-attribute exposures." if block_given?
       end
+
+      options[:proc] = block if block_given?
 
       args.each do |attribute|
         exposures[attribute.to_sym] = options
@@ -108,9 +117,10 @@ module Grape
     # @param options [Hash] Any options you pass in here will be known to the entity
     #   representation, this is where you can trigger things from conditional options
     #   etc.
-    def serializable_hash(options = {})
+    def serializable_hash(runtime_options = {})
+      opts = options.merge(runtime_options)
       exposures.inject({}) do |output, (attribute, exposure_options)|
-        output[key_for(attribute)] = value_for(attribute) if conditions_met?(exposure_options, options)
+        output[key_for(attribute)] = value_for(attribute, opts) if conditions_met?(exposure_options, opts)
         output
       end
     end
@@ -123,10 +133,12 @@ module Grape
       exposures[attribute.to_sym][:as] || attribute.to_sym
     end
 
-    def value_for(attribute)
+    def value_for(attribute, options = {})
       exposure_options = exposures[attribute.to_sym]
 
-      if exposure_options[:using]
+      if exposure_options[:proc]
+        exposure_options[:proc].call(object, options)
+      elsif exposure_options[:using]
         exposure_options[:using].represent(object.send(attribute))
       else
         object.send(attribute)
