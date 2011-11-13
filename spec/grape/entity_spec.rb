61fd0a077cf0d48ccee1f04e88a562497e63f528
Michael Bleigh <michael@intridea.com>
2011-11-13 19:07:25 +0800

    Entity#serializable_hash works when nil is passed in. Closes #71

--- spec/grape/entity_spec.rb
+++ spec/grape/entity_spec.rb
@@ -87,6 +87,9 @@ describe Grape::Entity do
     subject{ fresh_class.new(model) }
 
     describe '#serializable_hash' do
+      it 'should not throw an exception if a nil options object is passed' do
+        expect{ fresh_class.new(model).serializable_hash(nil) }.not_to raise_error
+      end
     end
 
     describe '#value_for' do
