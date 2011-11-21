bc4b63f25dd374cbbd42690eb2c195be25238737
Jerry Cheung <jch@whatcodecraves.com>
2011-11-21 01:56:12 -0500

    shared example group for testing versions across strategies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'support'))
 
 $stdout = StringIO.new
 
