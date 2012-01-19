56dae8b0ba68d66457bbb3903447e58e8c2d67e5
Joey Aghion <joey@aghion.com>
2012-01-19 16:28:17 -0500

    allow APIs to declare additional content-types

--- lib/grape/api.rb
+++ lib/grape/api.rb
@@ -122,6 +122,12 @@ module Grape
         new_format ? set(:error_format, new_format.to_sym) : settings[:error_format]
       end
 
+      # Specify additional content-types, e.g.:
+      #   content_types :xls => 'application/vnd.ms-excel'
+      def content_types(map = {})
+        map.any? ? set(:content_types, map) : settings[:content_types]
+      end
+
       # Specify the default status code for errors.
       def default_error_status(new_status = nil)
         new_status ? set(:default_error_status, new_status) : settings[:default_error_status]
