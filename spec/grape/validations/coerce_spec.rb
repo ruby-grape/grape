bc3318ced6ac18b624c447fa34572af10ea9712b
Julien Ammous <schmurfy@gmail.com>
2012-07-11 22:16:09 +0200

    extracted validator tests in their own fiels

---
+++ spec/grape/validations/coerce_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Grape::Validations::CoerceValidator do
+  def app; @app; end
+  
+  before do
+    @app = Class.new(Grape::API) do
+      default_format :json
+      
+      params do
+        requires :int, :coerce => Integer
+        optional :arr, :coerce => Array[Integer]
+        optional :bool, :coerce => Array[Boolean]
+      end
+      get '/coerce' do
+        {
+          :int    => params[:int].class,
+          :arr    => params[:arr] ? params[:arr][0].class : nil,
+          :bool   => params[:bool] ? (params[:bool][0] == true) && (params[:bool][1] == false) : nil
+        }
+      end
+      
+    end
+    
+  end
+  
+  # TOOD: Later when virtus can tell us that an input IS invalid
+  # it "should return an error on malformed input" do
+  #   get '/coerce', :int => "43a"
+  #   last_response.status.should == 400
+  # end
+  
+  it 'should coerce inputs' do
+    get('/coerce', :int => "43")
+    last_response.status.should == 200
+    ret = MultiJson.load(last_response.body)
+    ret["int"].should == "Fixnum"
+    
+    get('/coerce', :int => "40", :arr => ["1","20","3"], :bool => [1, 0])
+    last_response.status.should == 200
+    ret = MultiJson.load(last_response.body)
+    ret["int"].should == "Fixnum"
+    ret["arr"].should == "Fixnum"
+    ret["bool"].should == true
+  end
+  
+end
--- spec/grape/validations_spec.rb
+++ spec/grape/validations_spec.rb
@@ -7,15 +7,6 @@ describe Grape::Validations do
     @app = Class.new(Grape::API) do
       default_format :json
       
-      
-      params do
-        requires :id, :regexp => /^[0-9]+$/
-      end
-      
-      post do
-        {:ret => params[:id]}
-      end
-      
       params do
         requires :name, :company
         optional :a_number, :regexp => /^[0-9]+$/
@@ -25,49 +16,8 @@ describe Grape::Validations do
         "Hello"
       end
       
-      params do
-        requires :int, :coerce => Integer
-        optional :arr, :coerce => Array[Integer]
-        optional :bool, :coerce => Array[Boolean]
-      end
-      get '/coerce' do
-        {
-          :int    => params[:int].class,
-          :arr    => params[:arr] ? params[:arr][0].class : nil,
-          :bool   => params[:bool] ? (params[:bool][0] == true) && (params[:bool][1] == false) : nil
-        }
-      end
-      
     end
     
-  end
-  
-  it 'validates id' do
-    post('/')
-    last_response.status.should == 400
-    last_response.body.should == "missing parameter: id"
-    
-    post('/', {}, 'rack.input' => StringIO.new('{"id" : "a56b"}'))
-    last_response.body.should == 'invalid parameter: id'
-    last_response.status.should == 400
-    
-    post('/', {}, 'rack.input' => StringIO.new('{"id" : 56}'))
-    last_response.body.should == '{"ret":56}'
-    last_response.status.should == 201
-  end
-  
-  it 'validates name, company' do
-    get('/')
-    last_response.status.should == 400
-    last_response.body.should == "missing parameter: name"
-    
-    get('/', :name => "Bob")
-    last_response.status.should == 400
-    last_response.body.should == "missing parameter: company"
-    
-    get('/', :name => "Bob", :company => "TestCorp")
-    last_response.status.should == 200
-    last_response.body.should == "Hello"
   end
   
   it 'validates optional parameter if present' do
@@ -80,18 +30,4 @@ describe Grape::Validations do
     last_response.body.should == "Hello"
   end
   
-  it 'should coerce inputs' do
-    get('/coerce', :int => "43")
-    last_response.status.should == 200
-    ret = MultiJson.load(last_response.body)
-    ret["int"].should == "Fixnum"
-    
-    get('/coerce', :int => "40", :arr => ["1","20","3"], :bool => [1, 0])
-    last_response.status.should == 200
-    ret = MultiJson.load(last_response.body)
-    ret["int"].should == "Fixnum"
-    ret["arr"].should == "Fixnum"
-    ret["bool"].should == true
-  end
-
 end
