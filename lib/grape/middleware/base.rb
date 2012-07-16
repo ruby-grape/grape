67250db4363d344975a77f367ed860698a4520c3
Ben Rosenblum <brosenblum@primerevenue.com>
2012-07-16 15:48:09 -0400

    cleanup recursive json serialization code per comments from dblock

--- lib/grape/middleware/base.rb
+++ lib/grape/middleware/base.rb
@@ -110,13 +110,19 @@ module Grape
           MultiJson.load(object)
         end
 
-        def serialize_recurse(object)
+        def serializable?(object)
+         object.respond_to?(:serializable_hash) ||
+           object.kind_of?(Array) && !object.map {|o| o.respond_to? :serializable_hash }.include?(false) ||
+           object.kind_of?(Hash)
+        end
+
+        def serialize(object)
           if object.respond_to? :serializable_hash
             object.serializable_hash
           elsif object.kind_of?(Array) && !object.map {|o| o.respond_to? :serializable_hash }.include?(false)
             object.map {|o| o.serializable_hash }
           elsif object.kind_of?(Hash)
-            object.inject({}) { |h,(k,v)| h[k] = serialize_recurse(v); h }
+            object.inject({}) { |h,(k,v)| h[k] = serialize(v); h }
           else
             object
           end
@@ -124,13 +130,10 @@ module Grape
 
         def encode_json(object)
           return object if object.is_a?(String)
+          return MultiJson.dump(serialize(object)) if serializable?(object)
+          return object.to_json if object.respond_to?(:to_json)
 
-          serialized_hash = serialize_recurse(object)
-          if (object == serialized_hash) && (object.respond_to? :to_json)
-            object.to_json
-          else
-            MultiJson.dump(serialized_hash)
-          end
+          MultiJson.dump(object)
         end
 
         def encode_txt(object)
