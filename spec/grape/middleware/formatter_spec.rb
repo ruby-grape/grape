ecb34ccb954449b65ee56be167b573e9de04ee5d
matt <matt.panaro@gmail.com>
2012-11-19 19:30:42 -0500

    add custom content type to custom content test in formatter_spec

--- spec/grape/middleware/formatter_spec.rb
+++ spec/grape/middleware/formatter_spec.rb
@@ -67,11 +67,11 @@ describe Grape::Middleware::Formatter do
           "<bar/>"
         end
       end
-
       subject.call({'PATH_INFO' => '/somewhere.xml', 'HTTP_ACCEPT' => 'application/json'}).last.each{|b| b.should == '<bar/>'}
     end
 
-    it "should call #to_custom if the content type is custom and the object responds to 'to_custom'" do
+    it "should call #to_custom on the object (if it responds) and the content type is 'custom'" do
+      subject.options[:content_types][:custom] = 'application/custom'
       @body = "string"
       @body.instance_eval do
         def to_custom
