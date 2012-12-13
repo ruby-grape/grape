ee76762eb6445e8c69561e9387a4c929ff8ff03a
Nikalyukin Mikhail <idups1k@gmail.com>
2012-12-13 19:05:53 +0200

    remove debug output

--- lib/grape/error_formatter/xml.rb
+++ lib/grape/error_formatter/xml.rb
@@ -4,14 +4,10 @@ module Grape
       class << self
 
         def call(message, backtrace, options = {})
-          p message
-          p backtrace
-
           result = message.is_a?(Hash) ? message : { :message => message }
           if (options[:rescue_options] || {})[:backtrace] && backtrace && ! backtrace.empty?
             result = result.merge({ :backtrace => backtrace })
           end
-          p result
           result.respond_to?(:to_xml) ? result.to_xml(:root => :error) : result.to_s
         end
 
