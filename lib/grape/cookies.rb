df8665cc874d8d5db17d0a7d13527d614b29fefb
dblock <dblock@dblock.org>
2012-12-27 23:44:19 -0500

    Fix: don't use to_sym for cookie parsing, leaks.

--- lib/grape/cookies.rb
+++ lib/grape/cookies.rb
@@ -1,41 +1,44 @@
+require 'active_support/core_ext/hash/indifferent_access'
+
 module Grape
   class Cookies
 
-      def initialize
-        @cookies = {}
-        @send_cookies = {}
-      end
+    def initialize
+      @cookies = HashWithIndifferentAccess.new
+      @send_cookies = HashWithIndifferentAccess.new
+    end
 
-      def read(request)
-        request.cookies.each do |name, value|
-          @cookies[name.to_sym] = value
-        end
-      end
-
-      def write(header)
-        @cookies.select { |key, value|
-            @send_cookies[key.to_sym] == true
-        }.each { |name, value|
-          Rack::Utils.set_cookie_header!(
-            header, name, value.instance_of?(Hash) ? value : { :value => value })
-        }
-      end
-
-      def [](name)
-        @cookies[name]
-      end
-
-      def []=(name, value)
-        @cookies[name.to_sym] = value
-        @send_cookies[name.to_sym] = true
-      end
-
-      def each(&block)
-        @cookies.each(&block)
-      end
-
-      def delete(name)
-        self.[]=(name, { :value => 'deleted', :expires => Time.at(0) })
+    def read(request)
+      request.cookies.each do |name, value|
+        @cookies[name.to_s] = value
       end
     end
+
+    def write(header)
+      @cookies.select { |key, value|
+        @send_cookies[key] == true
+      }.each { |name, value|
+        cookie_value = value.is_a?(Hash) ? value : { :value => value }
+        Rack::Utils.set_cookie_header! header, name, cookie_value
+      }
+    end
+
+    def [](name)
+      @cookies[name]
+    end
+
+    def []=(name, value)
+      @cookies[name] = value
+      @send_cookies[name] = true
+    end
+
+    def each(&block)
+      @cookies.each(&block)
+    end
+
+    def delete(name)
+      self.[]=(name, { :value => 'deleted', :expires => Time.at(0) })
+    end
+
+  end
 end
