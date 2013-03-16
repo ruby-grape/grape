ruby-grape/grape

commit 48d05b50ec5351fdfde6a68b37d69c58a73c2381

Potential fix for wrong execution order of Rack Middleware

lib/grape/endpoint.rb
@@ -389,6 +389,18 @@ module Grape
     def build_middleware
       b = Rack::Builder.new
 
+
+      aggregate_setting(:middleware).each do |m|
+        m = m.dup
+        block = m.pop if m.last.is_a?(Proc)
+        if block
+          b.use *m, &block
+        else
+          b.use *m
+        end
+      end
+
+
       b.use Rack::Head
       b.use Grape::Middleware::Error,
         :default_status => settings[:default_error_status] || 403,
@@ -417,15 +429,6 @@ module Grape
         :formatters => settings[:formatters],
         :parsers => settings[:parsers]
 
-      aggregate_setting(:middleware).each do |m|
-        m = m.dup
-        block = m.pop if m.last.is_a?(Proc)
-        if block
-          b.use *m, &block
-        else
-          b.use *m
-        end
-      end
 
       b
     end

commit 4114caa4ffe4cd8d7f489b3d159ee065c4921a42

Fix: mount Grape middleware behind Error to allow error trapping, introduced in 48d05b50ec5351fdfde6a68b37d69c58a73c2381.

lib/grape/endpoint.rb
@@ -393,16 +393,6 @@ module Grape
     def build_middleware
       b = Rack::Builder.new
 
-      aggregate_setting(:middleware).each do |m|
-        m = m.dup
-        block = m.pop if m.last.is_a?(Proc)
-        if block
-          b.use *m, &block
-        else
-          b.use *m
-        end
-      end
-
       b.use Rack::Head
       b.use Grape::Middleware::Error,
         :default_status => settings[:default_error_status] || 403,
@@ -412,6 +402,16 @@ module Grape
         :error_formatters => settings[:error_formatters],
         :rescue_options => settings[:rescue_options],
         :rescue_handlers => merged_setting(:rescue_handlers)
+
+      aggregate_setting(:middleware).each do |m|
+        m = m.dup
+        block = m.pop if m.last.is_a?(Proc)
+        if block
+          b.use *m, &block
+        else
+          b.use *m
+        end
+      end
 
       b.use Rack::Auth::Basic, settings[:auth][:realm], &settings[:auth][:proc] if settings[:auth] && settings[:auth][:type] == :http_basic
       b.use Rack::Auth::Digest::MD5, settings[:auth][:realm], settings[:auth][:opaque], &settings[:auth][:proc] if settings[:auth] && settings[:auth][:type] == :http_digest

spec/grape/api_spec.rb
@@ -668,6 +668,20 @@ describe Grape::API do
           last_response.body.should == 'true'
         end
       end
+
+      it 'mounts behind error middleware' do
+        m = Class.new(Grape::Middleware::Base) do
+          def before
+            throw :error, message: "Caught in the Net", status: 400
+          end
+        end
+        subject.use m
+        subject.get "/" do
+        end
+        get "/"
+        last_response.status.should == 400
+        last_response.body.should == "Caught in the Net"
+      end
     end
   end
   describe '.basic' do