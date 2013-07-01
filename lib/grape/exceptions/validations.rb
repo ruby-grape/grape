36894d7c8e06d4f555c4b055c5062cef12919bd8
Raffael Schmid <raffael@yux.ch>
2013-07-01 14:08:22 +0200

    Split long line

--- lib/grape/exceptions/validations.rb
+++ lib/grape/exceptions/validations.rb
@@ -7,7 +7,8 @@ module Grape
 
       def initialize(args = {})
         @errors = args[:errors]
-        super message: @errors.collect { |e| e.message }.join(', '), status: 400
+        errors_as_sentence = @errors.collect { |e| e.message }.join(', ')
+        super message: errors_as_sentence, status: 400
       end
     end
   end
