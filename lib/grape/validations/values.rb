797460b4e972df01698aa4b015d2801993d0d978
Danny Hawkins <danny.hawkins@gmail.com>
2014-01-24 13:07:47 +0000

    moved to initialize

--- lib/grape/validations/values.rb
+++ lib/grape/validations/values.rb
@@ -2,13 +2,13 @@ module Grape
   module Validations
     class ValuesValidator < Validator
       def initialize(attrs, options, required, scope)
-        @values = options
+        @values = (options.is_a?(Proc) ? options.call : options)
         @required = required
         super
       end
 
       def validate_param!(attr_name, params)
-        if (params[attr_name] || @required) && !(@values.is_a?(Proc) ? @values.call : @values).include?(params[attr_name])
+        if (params[attr_name] || @required) && !@values.include?(params[attr_name])
           raise Grape::Exceptions::Validation, param: @scope.full_name(attr_name), message_key: :values
         end
       end
