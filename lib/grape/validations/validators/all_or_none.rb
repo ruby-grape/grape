d59e8b038fe90a582a3293b65401913b7ca57b80
lizihe <loveltyoic@gmail.com>
2014-11-06 15:06:17 +0800

    Add all_or_none validator

---
+++ lib/grape/validations/validators/all_or_none.rb
@@ -0,0 +1,20 @@
+module Grape
+  module Validations
+    require 'grape/validations/validators/multiple_params_base'
+    class AllOrNoneOfValidator < MultipleParamsBase
+      def validate!(params)
+        super
+        if scope_requires_params && only_subset_present
+          raise Grape::Exceptions::Validation, params: all_keys, message_key: :all_or_none
+        end
+        params
+      end
+
+      private
+
+      def only_subset_present
+        scoped_params.any? { |resource_params| keys_in_common(resource_params).length > 0 && keys_in_common(resource_params).length < attrs.length  }
+      end
+    end
+  end
+end
