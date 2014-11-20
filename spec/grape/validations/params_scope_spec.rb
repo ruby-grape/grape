64d75b0fd5585fecbc93dc6a0d807d3531861084
Matthew Zikherman <matt@bestvendor.com>
2014-11-19 19:11:31 -0500

    Allow both desc and description in the parameter DSL.

--- spec/grape/validations/params_scope_spec.rb
+++ spec/grape/validations/params_scope_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Grape::Validations::ParamsScope do
+  subject do
+    Class.new(Grape::API)
+  end
+
+  def app
+    subject
+  end
+
+  context 'setting description' do
+    [:desc, :description].each do |description_type|
+      it "allows setting #{description_type}" do
+        subject.params do
+          requires :int, type: Integer, description_type => 'My very nice integer'
+        end
+        subject.get '/single' do
+          'int works'
+        end
+        get '/single', int: 420
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to eq('int works')
+      end
+    end
+  end
 end
