5c595e2b120a5c8a2b5c2d16313a6c951a5e96bf
dB <dblock@dblock.org>
2014-12-28 09:38:22 -0500

    Spec from #857, confirm fix for included helper modules no longer available in nested mounted resources.

---
+++ spec/grape/api/nested_helpers_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Grape::API::Helpers do
+  subject do
+    module HelperMethods
+      extend Grape::API::Helpers
+      def current_user
+        @current_user ||= params[:current_user]
+      end
+    end
+
+    class Nested < Grape::API
+      resource :level1 do
+        helpers HelperMethods
+
+        get do
+          current_user
+        end
+
+        resource :level2 do
+          get do
+            current_user
+          end
+        end
+      end
+    end
+
+    class Main < Grape::API
+      mount Nested
+    end
+
+    Main
+  end
+
+  def app
+    subject
+  end
+
+  it 'can access helpers from a mounted resource' do
+    get '/level1', current_user: 'hello'
+    expect(last_response.body).to eq('hello')
+  end
+
+  it 'can access helpers from a mounted resource in a nested resource' do
+    get '/level1/level2', current_user: 'world'
+    expect(last_response.body).to eq('world')
+  end
+end
