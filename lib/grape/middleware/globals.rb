f9e52fc0cbadee127862cf40403fc185350a434d
Edward Rudd <urkle@outoforder.cc>
2015-04-29 09:41:46 -0400

    fix the Globals middleware
    - add to the autoload configuration
    - fix issues with it accessing an undefined var/method

--- lib/grape/middleware/globals.rb
+++ lib/grape/middleware/globals.rb
@@ -4,7 +4,8 @@ module Grape
   module Middleware
     class Globals < Base
       def before
-        @env['grape.request'] = Grape::Request.new(@env)
+        request = Grape::Request.new(@env)
+        @env['grape.request'] = request
         @env['grape.request.headers'] = request.headers
         @env['grape.request.params'] = request.params if @env['rack.input']
       end
---
+++ spec/grape/middleware/globals_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Grape::Middleware::Globals do
+  subject { Grape::Middleware::Globals.new(blank_app) }
+  before { allow(subject).to receive(:dup).and_return(subject) }
+
+  let(:blank_app) { lambda { |env| [200, {}, 'Hi there.'] } }
+
+  it 'calls through to the app' do
+    expect(subject.call({})).to eq([200, {}, 'Hi there.'])
+  end
+
+  context 'environment' do
+    it 'should set the grape.request environment' do
+      subject.call({})
+      expect(subject.env['grape.request']).to be_a(Grape::Request)
+    end
+    it 'should set the grape.request.headers environment' do
+      subject.call({})
+      expect(subject.env['grape.request.headers']).to be_a(Hash)
+    end
+    it 'should set the grape.request.params environment' do
+      subject.call('QUERY_STRING' => 'test=1', 'rack.input' => StringIO.new)
+      expect(subject.env['grape.request.params']).to be_a(Hash)
+    end
+  end
+end
