ead7c0c19ec4d8a6d02630050319a7bea9008a93
Balasankar C <balasankarc@autistici.org>
2015-07-14 19:53:00 +0530

    Revert "Remove unwanted test"
    This reverts commit d9a0d227ac4ba9f399cc8e221c49da83d79bcf30.

--- spec/grape/integration/rack_spec.rb
+++ spec/grape/integration/rack_spec.rb
@@ -18,6 +18,12 @@ describe Rack do
         'CONTENT_TYPE' => 'application/json'
       }
       env = Rack::MockRequest.env_for('/', options)
+
+      unless RUBY_PLATFORM == 'java'
+        major, minor, release = Rack.release.split('.').map(&:to_i)
+        pending 'Rack 1.5.3 or 1.6.1 required' unless major >= 1 && ((minor == 5 && release >= 3) || (minor >= 6))
+      end
+
       expect(JSON.parse(app.call(env)[2].body.first)['params_keys']).to match_array('test')
     ensure
       input.close
