4569f1fc16848addf518a7868d9d48d0157d22f6
aaron <aaron@nonobo.com>
2016-01-23 07:39:46 +0800

    modify spec

--- spec/grape/validations_spec.rb
+++ spec/grape/validations_spec.rb
@@ -649,11 +649,11 @@ describe Grape::Validations do
 
       it 'should prompt index in array when a parameter is not present' do
         put_with_json '/within_array', children: [
-          { name: 'Job', parents: [{ name: 'Joy' }, { name: 'Bob' }] },
-          { name: 'Jim', parents: [{}] }
+          { name: 'Job', parents: [{ name: 'Joy' }, {}] },
+          { name: 'Jim', parents: [{ name: 'Bob' }] }
         ]
         expect(last_response.status).to eq(400)
-        expect(last_response.body).to eq('children[0][parents][0][name] is missing')
+        expect(last_response.body).to eq('children[0][parents][1][name] is missing')
 
         put_with_json '/within_array', children: [
           { name: 'Job', parents: [{ name: 'Joy' }] },
