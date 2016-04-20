b8d87128e0621790304ab9ecfa1f4f709c6a08cd
Marieke GUEYE <marieke@pryntcases.com>
2016-04-20 13:41:19 -0700

    Store back `message_key` on `Grape::Exceptions::Validation`

--- lib/grape/exceptions/validation.rb
+++ lib/grape/exceptions/validation.rb
@@ -9,6 +9,7 @@ module Grape
       def initialize(args = {})
         fail 'Params are missing:' unless args.key? :params
         @params = args[:params]
+        @message_key = args[:message] if args.key?(:message) && args[:message].is_a?(Symbol)
         args[:message] = translate_message(args[:message]) if args.key? :message
         super
       end
