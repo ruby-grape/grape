2a032dfc34c25854d823711c6c37a9671ef43430
Glauco Custódio <glauco.custodio@gmail.com>
2016-05-05 21:44:30 -0300

    Fix rubocop

--- lib/grape/error_formatter/xml.rb
+++ lib/grape/error_formatter/xml.rb
@@ -9,7 +9,7 @@ module Grape
 
           result = message.is_a?(Hash) ? message : { message: message }
           if (options[:rescue_options] || {})[:backtrace] && backtrace && !backtrace.empty?
-            result.merge!(backtrace: backtrace)
+            result[:backtrace] = backtrace
           end
           result.respond_to?(:to_xml) ? result.to_xml(root: :error) : result.to_s
         end
