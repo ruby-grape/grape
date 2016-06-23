c83c41f5c88cf256670e571c7ea1f931e0bbc3a8
Andrew Rempe <andrewrempe@gmail.com>
2016-06-23 12:27:16 -0400

    Fix rubocop errors in activesupport hash patching

--- lib/grape/core_ext/hash.rb
+++ lib/grape/core_ext/hash.rb
@@ -1,13 +1,23 @@
 class Hash
   def deep_symbolize_keys
-    _deep_transform_keys_in_object(self) { |key| key.to_sym rescue key }
+    _deep_transform_keys_in_object(self) do |key|
+      begin
+       key.to_sym
+     rescue
+       key
+     end
+    end
   end unless Hash.method_defined?(:deep_symbolize_keys)
 
   def deep_symbolize_keys!
-    _deep_transform_keys_in_object!(self) { |key| key.to_sym rescue key }
+    _deep_transform_keys_in_object!(self) do |key|
+      begin
+        key.to_sym
+      rescue
+        key
+      end
+    end
   end unless Hash.method_defined?(:deep_symbolize_keys!)
-
-  private
 
   def _deep_transform_keys_in_object(object, &block)
     case object
@@ -16,7 +26,7 @@ class Hash
         result[yield(key)] = _deep_transform_keys_in_object(value, &block)
       end
     when Array
-      object.map {|e| _deep_transform_keys_in_object(e, &block) }
+      object.map { |e| _deep_transform_keys_in_object(e, &block) }
     else
       object
     end
@@ -31,7 +41,7 @@ class Hash
       end
       object
     when Array
-      object.map! {|e| _deep_transform_keys_in_object!(e, &block)}
+      object.map! { |e| _deep_transform_keys_in_object!(e, &block) }
     else
       object
     end
