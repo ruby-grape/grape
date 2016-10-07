f54f9b456d1a85df2ad0881afec98f8efff148d3
Alexander Koltun <Alexander.Koltun@vasco.com>
2016-10-07 09:35:46 +0200

    Fix: Change :has_key? to :key? according rubocop

--- lib/grape/validations/validators/regexp.rb
+++ lib/grape/validations/validators/regexp.rb
@@ -2,7 +2,7 @@ module Grape
   module Validations
     class RegexpValidator < Base
       def validate_param!(attr_name, params)
-        return unless params.respond_to?(:has_key?) && params.has_key?(attr_name)
+        return unless params.respond_to?(:key?) && params.key?(attr_name)
         return if Array.wrap(params[attr_name]).all? { |param| param.nil? || (param.to_s =~ (options_key?(:value) ? @option[:value] : @option)) }
         raise Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: message(:regexp)
       end
