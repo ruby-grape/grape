2491071ff293b900e65b2cc8fb044edd1c25ae80
James McCarthy <james@thisishatch.co.uk>
2017-03-16 16:44:42 +0700

    Add build_parameters_with to Grape::Middleware::Globals

--- lib/grape/middleware/globals.rb
+++ lib/grape/middleware/globals.rb
@@ -4,7 +4,7 @@ module Grape
   module Middleware
     class Globals < Base
       def before
-        request = Grape::Request.new(@env)
+        request = Grape::Request.new(@env, build_params_with: @options[:build_params_with])
         @env[Grape::Env::GRAPE_REQUEST] = request
         @env[Grape::Env::GRAPE_REQUEST_HEADERS] = request.headers
         @env[Grape::Env::GRAPE_REQUEST_PARAMS] = request.params if @env[Grape::Env::RACK_INPUT]
