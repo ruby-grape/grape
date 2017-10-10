ruby-grape/grape

commit f34fe1261c29b171cfc2c6a935d0cea8b307beff

Bugfix: Correctly handle `given` in Array params (#1625)
* Bugfix: Correctly handle `given` in Array params

Array parameters are handled as a parameter that opens a
scope with `type: Array`; `given` opens up a new scope, but
was always setting the type to Hash. This patch fixes that,
as well as correctly labeling the error messages associated
with array fields.

* Code review fix

* Add CHANGELOG entry

spec/grape/validations/params_scope_spec.rb
@@ -473,6 +473,29 @@ describe Grape::Validations::ParamsScope do
     end
   end
 
+  context 'when validations are dependent on a parameter within an array param' do
+    before do
+      subject.params do
+        requires :foos, type: Array do
+          optional :foo_type, :baz_type
+          given :foo_type do
+            requires :bar
+          end
+        end
+      end
+      subject.post('/test') { declared(params).to_json }
+    end
+
+    it 'applies the constraint within each value' do
+      post '/test',
+           { foos: [{ foo_type: 'a' }, { baz_type: 'c' }] }.to_json,
+           'CONTENT_TYPE' => 'application/json'
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq('foos[0][bar] is missing')
+    end
+  end
+
   context 'when validations are dependent on a parameter with specific value' do
     # build test cases from all combinations of declarations and options
     a_decls = %i(optional requires)

lib/grape/validations/params_scope.rb
@@ -42,37 +42,45 @@ module Grape
         return false if @optional && (params(parameters).blank? ||
                                        any_element_blank?(parameters))
 
-        @dependent_on.each do |dependency|
-          if dependency.is_a?(Hash)
-            dependency_key = dependency.keys[0]
-            proc = dependency.values[0]
-            return false unless proc.call(params(parameters).try(:[], dependency_key))
-          elsif params(parameters).try(:[], dependency).blank?
-            return false
-          end
-        end if @dependent_on
-
         return true if parent.nil?
         parent.should_validate?(parameters)
       end
 
+      def meets_dependency?(params)
+        return true unless @dependent_on
+
+        params = params.with_indifferent_access
+
+        @dependent_on.each do |dependency|
+          if dependency.is_a?(Hash)
+            dependency_key = dependency.keys[0]
+            proc = dependency.values[0]
+            return false unless proc.call(params.try(:[], dependency_key))
+          elsif params.respond_to?(:key?) && params.try(:[], dependency).blank?
+            return false
+          end
+        end
+
+        true
+      end
+
       # @return [String] the proper attribute name, with nesting considered.
-      def full_name(name)
+      def full_name(name, index: nil)
         if nested?
           # Find our containing element's name, and append ours.
-          "#{@parent.full_name(@element)}#{array_index}[#{name}]"
+          [@parent.full_name(@element), [@index || index, name].map(&method(:brackets))].compact.join
         elsif lateral?
-          # Find the name of the element as if it was at the
-          # same nesting level as our parent.
-          @parent.full_name(name)
+          # Find the name of the element as if it was at the same nesting level
+          # as our parent. We need to forward our index upward to achieve this.
+          @parent.full_name(name, index: @index)
         else
           # We must be the root scope, so no prefix needed.
           name.to_s
         end
       end
 
-      def array_index
-        "[#{@index}]" if @index.present?
+      def brackets(val)
+        "[#{val}]" if val
       end
 
       # @return [Boolean] whether or not this scope is the root-level scope
@@ -187,7 +195,7 @@ module Grape
           element:      nil,
           parent:       self,
           options:      @optional,
-          type:         Hash,
+          type:         type == Array ? Array : Hash,
           dependent_on: options[:dependent_on],
           &block
         )

lib/grape/validations/validators/base.rb
@@ -41,6 +41,7 @@ module Grape
         array_errors = []
         attributes.each do |resource_params, attr_name|
           next unless @required || (resource_params.respond_to?(:key?) && resource_params.key?(attr_name))
+          next unless @scope.meets_dependency?(resource_params)
 
           begin
             validate_param!(attr_name, resource_params)

commit fe56f1596d36309622afe794774b8fe448ce771d

Fix validations nested under a `given`
Multi-param and default validators, all of which implemented their
own `validate!` method, were not respecting the `given` dependency
as expected. This change copies the check into all those validators.

The bug was likely introduced by #1625, which required that the
dependency check be moved out of the scope itself and into the
validators. Rolling back that change would require fundamentally
re-architecting how array parameters are validated (possibly with
a new kind of scope).

lib/grape/validations/validators/all_or_none.rb
@@ -4,7 +4,7 @@ module Grape
     class AllOrNoneOfValidator < MultipleParamsBase
       def validate!(params)
         super
-        if scope_requires_params && only_subset_present
+        if scope_requires_params && only_subset_present(params)
           raise Grape::Exceptions::Validation, params: all_keys, message: message(:all_or_none)
         end
         params
@@ -12,8 +12,12 @@ module Grape
 
       private
 
-      def only_subset_present
-        scoped_params.any? { |resource_params| !keys_in_common(resource_params).empty? && keys_in_common(resource_params).length < attrs.length }
+      def only_subset_present(params)
+        scoped_params.any? do |resource_params|
+          next unless scope_should_validate?(resource_params, params)
+
+          !keys_in_common(resource_params).empty? && keys_in_common(resource_params).length < attrs.length
+        end
       end
     end
   end

lib/grape/validations/validators/at_least_one_of.rb
@@ -4,7 +4,7 @@ module Grape
     class AtLeastOneOfValidator < MultipleParamsBase
       def validate!(params)
         super
-        if scope_requires_params && no_exclusive_params_are_present
+        if scope_requires_params && no_exclusive_params_are_present(params)
           raise Grape::Exceptions::Validation, params: all_keys, message: message(:at_least_one)
         end
         params
@@ -12,8 +12,12 @@ module Grape
 
       private
 
-      def no_exclusive_params_are_present
-        scoped_params.any? { |resource_params| keys_in_common(resource_params).empty? }
+      def no_exclusive_params_are_present(params)
+        scoped_params.any? do |resource_params|
+          next unless scope_should_validate?(resource_params, params)
+
+          keys_in_common(resource_params).empty?
+        end
       end
     end
   end

lib/grape/validations/validators/default.rb
@@ -20,6 +20,8 @@ module Grape
       def validate!(params)
         attrs = AttributesIterator.new(self, @scope, params)
         attrs.each do |resource_params, attr_name|
+          next unless @scope.meets_dependency?(resource_params, params)
+
           if resource_params.is_a?(Hash) && resource_params[attr_name].nil?
             validate_param!(attr_name, resource_params)
           end

lib/grape/validations/validators/exactly_one_of.rb
@@ -4,7 +4,7 @@ module Grape
     class ExactlyOneOfValidator < MutualExclusionValidator
       def validate!(params)
         super
-        if scope_requires_params && none_of_restricted_params_is_present
+        if scope_requires_params && none_of_restricted_params_is_present(params)
           raise Grape::Exceptions::Validation, params: all_keys, message: message(:exactly_one)
         end
         params
@@ -21,8 +21,12 @@ module Grape
 
       private
 
-      def none_of_restricted_params_is_present
-        scoped_params.any? { |resource_params| keys_in_common(resource_params).empty? }
+      def none_of_restricted_params_is_present(params)
+        scoped_params.any? do |resource_params|
+          next unless scope_should_validate?(resource_params, params)
+
+          keys_in_common(resource_params).empty?
+        end
       end
     end
   end

lib/grape/validations/validators/multiple_params_base.rb
@@ -10,6 +10,10 @@ module Grape
 
       private
 
+      def scope_should_validate?(scoped_params, params)
+        @scope.meets_dependency?(scoped_params, params)
+      end
+
       def scope_requires_params
         @scope.required? || scoped_params.any?(&:any?)
       end

lib/grape/validations/validators/mutual_exclusion.rb
@@ -6,7 +6,7 @@ module Grape
 
       def validate!(params)
         super
-        if two_or_more_exclusive_params_are_present
+        if two_or_more_exclusive_params_are_present(params)
           raise Grape::Exceptions::Validation, params: processing_keys_in_common, message: message(:mutual_exclusion)
         end
         params
@@ -14,8 +14,10 @@ module Grape
 
       private
 
-      def two_or_more_exclusive_params_are_present
+      def two_or_more_exclusive_params_are_present(params)
         scoped_params.any? do |resource_params|
+          next unless scope_should_validate?(resource_params, params)
+
           @processing_keys_in_common = keys_in_common(resource_params)
           @processing_keys_in_common.length > 1
         end

spec/grape/validations/params_scope_spec.rb
@@ -407,6 +407,60 @@ describe Grape::Validations::ParamsScope do
       expect(last_response.status).to eq(200)
     end
 
+    it 'sets default values only if the dependency is met' do
+      subject.params do
+        optional :a
+        given :a do
+          optional :b, default: 'default value'
+        end
+      end
+      subject.get('/defaults') { params.to_json }
+
+      get '/defaults'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('{}')
+
+      get '/defaults', a: true
+      expect(last_response.status).to eq(200)
+      expect(JSON.parse(last_response.body)).to eq('a' => 'true', 'b' => 'default value')
+    end
+
+    it 'conditionally applies all multi-param validators' do
+      subject.params do
+        optional :a
+        given :a do
+          optional :b, :c, :d, :e, :f, :g
+
+          exactly_one_of :b, :c
+          all_or_none_of :d, :e
+          at_least_one_of :f, :g
+        end
+      end
+      subject.get('/exactly_one') { params.to_json }
+
+      get '/exactly_one'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('{}')
+
+      get '/exactly_one', a: true, b: true, f: true
+      expect(last_response.status).to eq(200)
+
+      get '/exactly_one', a: true, c: true, f: true
+      expect(last_response.status).to eq(200)
+
+      get '/exactly_one', a: true, b: true, c: true, f: true
+      expect(last_response.status).to eq(400)
+
+      get '/exactly_one', d: true
+      expect(last_response.status).to eq(200)
+
+      get '/exactly_one', a: true, b: true, d: true, e: true, f: true
+      expect(last_response.status).to eq(200)
+
+      get '/exactly_one', a: true, b: true, f: true, g: true
+      expect(last_response.status).to eq(200)
+    end
+
     it 'applies only the appropriate validation' do
       subject.params do
         optional :a