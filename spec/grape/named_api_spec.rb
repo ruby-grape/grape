2891453f02992c840b05ef4275f45ad797e6fbeb
Nico <myxoh10@gmail.com>
2018-11-27 23:52:28 +0000

    uses stub_const rather than polluting the global scope

--- spec/grape/named_api_spec.rb
+++ spec/grape/named_api_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe 'A named API' do
-  NamedAPI = Class.new(Grape::API) do
-    get 'test' do
-      'response'
+  subject(:api_name) { NamedAPI.endpoints.last.options[:for].to_s }
+
+  let(:api) do
+    Class.new(Grape::API) do
+      get 'test' do
+        'response'
+      end
     end
   end
 
-  subject(:api_name) { NamedAPI.endpoints.last.options[:for].to_s }
+  before { stub_const('NamedAPI', api) }
 
   it 'can access the name of the API' do
     expect(api_name).to eq 'NamedAPI'
