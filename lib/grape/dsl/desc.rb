96a6e1ccd795a187207e5491ba98f191a33ccfdf
Fotos Georgiadis <gfotos@gmail.com>
2018-12-02 04:14:48 +0100

    Move summary documentation under options hash

--- lib/grape/dsl/desc.rb
+++ lib/grape/dsl/desc.rb
@@ -4,12 +4,12 @@ module Grape
       include Grape::DSL::Settings
 
       # Add a description to the next namespace or function.
-      # @option options :summary [String] summary for this endpoint
       # @param description [String] descriptive string for this endpoint
       #   or namespace
       # @param options [Hash] other properties you can set to describe the
       #   endpoint or namespace. Optional.
       # @option options :detail [String] additional detail about this endpoint
+      # @option options :summary [String] summary for this endpoint
       # @option options :params [Hash] param types and info. normally, you set
       #   these via the `params` dsl method.
       # @option options :entity [Grape::Entity] the entity returned upon a
