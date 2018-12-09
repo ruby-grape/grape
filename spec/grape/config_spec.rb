b64c636adb3002fb1bd8597b46de2735a00e5c32
Nico <myxoh10@gmail.com>
2018-12-09 23:26:03 +0000

    [Issue-1775] configures the param builder with a new value

--- spec/grape/config_spec.rb
+++ spec/grape/config_spec.rb
@@ -1,31 +1,17 @@
 require 'spec_helper'
 
 describe '.configure' do
-  let!(:grape_double) { Module.new { extend Grape::Config } }
-
-  context 'when not configured' do
-    it 'does not change when resetted' do
-      expect { grape_double.config.reset }
-        .not_to(change { grape_double.config.param_builder })
+  before do
+    Grape.configure do |config|
+      config.param_builder = 42
     end
   end
 
-  context 'when configured' do
-    subject(:configure) do
-      grape_double.configure do |config|
-        config.param_builder = 42
-      end
-    end
+  after do
+    Grape.config.reset
+  end
 
-    it 'changes the value' do
-      expect { configure }.to change { grape_double.config.param_builder }.to(42)
-    end
-
-    it 'can be restored by resetting' do
-      configure
-      expect { grape_double.config.reset }
-        .to change { grape_double.config.param_builder }
-        .from(42)
-    end
+  it 'is configured to the new value' do
+    expect(Grape.config.param_builder).to eq 42
   end
 end
