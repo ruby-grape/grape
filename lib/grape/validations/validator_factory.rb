bcba3acb0cbc5c90530b4c817522614a7f3d7cb0
Jean-Francis Bastien <bhacaz@gmail.com>
2019-07-25 13:32:59 -0400

    Read options validator_class in ValidatorFactory

--- lib/grape/validations/validator_factory.rb
+++ lib/grape/validations/validator_factory.rb
@@ -2,11 +2,11 @@ module Grape
   module Validations
     class ValidatorFactory
       def self.create_validator(**options)
-        options.delete(:validator_class).new(options[:attributes],
-                                             options[:options],
-                                             options[:required],
-                                             options[:params_scope],
-                                             options[:opts])
+        options[:validator_class].new(options[:attributes],
+                                      options[:options],
+                                      options[:required],
+                                      options[:params_scope],
+                                      options[:opts])
       end
     end
   end
