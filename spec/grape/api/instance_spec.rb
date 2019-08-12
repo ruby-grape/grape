ee9f504e57af883de25b0654f75e88477340b2d1
Jean-Francis Bastien <bhacaz@gmail.com>
2019-08-12 08:20:54 -0400

    Rewrite API::Instance spec

--- spec/grape/api/instance_spec.rb
+++ spec/grape/api/instance_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe Grape::API::Instance do
-  describe 'Boolean constant' do
-    subject { Class.new(described_class)::Boolean }
+  describe 'boolean constant' do
+    module InstanceSpec
+      class API < Grape::API
+        params do
+          requires :message, type: Boolean
+        end
+        post :echo do
+          params[:message]
+        end
+      end
+    end
+
+    def app
+      InstanceSpec::API
+    end
+
     it 'sets Boolean as a Virtus::Attribute::Boolean' do
-      is_expected.to eq Virtus::Attribute::Boolean
+      post '/echo?message=true'
+      expect(last_response.status).to eq(201)
     end
   end
 end
