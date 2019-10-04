96e4e1044e73e64414af709352b270ab4f88c864
Nick Bienko <bikolya@gmail.com>
2019-10-04 17:42:06 +0200

    Refactor exacly_one_of spec to use fake API

--- lib/grape/validations/validators/exactly_one_of.rb
+++ lib/grape/validations/validators/exactly_one_of.rb
@@ -1,28 +1,11 @@
 module Grape
   module Validations
-    require 'grape/validations/validators/mutual_exclusion'
-    class ExactlyOneOfValidator < MutualExclusionValidator
-      def validate!(params)
-        super
-        if scope_requires_params && none_of_restricted_params_is_present
+    require 'grape/validations/validators/multiple_params_base'
+    class ExactlyOneOfValidator < MultipleParamsBase
+      def validate_params!(params)
+        if keys_in_common(params).length != 1
           raise Grape::Exceptions::Validation, params: all_keys, message: message(:exactly_one)
         end
-        params
-      end
-
-      def message(default_key = nil)
-        options = instance_variable_get(:@option)
-        if options_key?(:message)
-          (options_key?(default_key, options[:message]) ? options[:message][default_key] : options[:message])
-        else
-          default_key
-        end
-      end
-
-      private
-
-      def none_of_restricted_params_is_present
-        scoped_params.any? { |resource_params| keys_in_common(resource_params).empty? }
       end
     end
   end
