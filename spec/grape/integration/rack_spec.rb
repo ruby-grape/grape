24925a2c61329f86c83fa9be73cdbdcbd1f78270
Nikolay Rys <nikolay@rys.me>
2019-10-13 23:53:55 +0200

    Remove test workaround for Rails 3
    Since rails_3 appraisal is dropped, the workaround(which wasn't implemented correctly either) can be removed.

--- spec/grape/integration/rack_spec.rb
+++ spec/grape/integration/rack_spec.rb
@@ -19,12 +19,6 @@ describe Rack do
       }
       env = Rack::MockRequest.env_for('/', options)
 
-      unless RUBY_PLATFORM == 'java'
-        major, minor, patch = Rack.release.split('.').map(&:to_i)
-        patch ||= 0 # rack <= 1.5.2 does not specify patch version
-        pending 'Rack 1.5.3 or 1.6.1 required' unless major >= 2 || (major >= 1 && ((minor == 5 && patch >= 3) || (minor >= 6)))
-      end
-
       expect(JSON.parse(app.call(env)[2].body.first)['params_keys']).to match_array('test')
     ensure
       input.close
