d99af69bf58dc512379f199165d360d757293425
Eric Proulx <eproulx@petalmd.com>
2020-03-05 00:42:41 +0100

    Extend Grape::ContentType with Util::Registrable Remove rubocop disable Style/MutableConstant Adapt spec

--- lib/grape/content_types.rb
+++ lib/grape/content_types.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 
+require 'grape/util/registrable'
+
 module Grape
   module ContentTypes
+    extend Util::Registrable
+
     # Content types are listed in order of preference.
-    CONTENT_TYPES = { # rubocop:disable Style/MutableConstant
+    CONTENT_TYPES = {
       xml: 'application/xml',
       serializable_hash: 'application/json',
       json: 'application/json',
       binary: 'application/octet-stream',
       txt: 'text/plain'
-    }
+    }.freeze
 
-    def self.content_types_for_settings(settings)
-      return if settings.blank?
+    class << self
+      def content_types_for_settings(settings)
+        return if settings.blank?
 
-      settings.each_with_object({}) { |value, result| result.merge!(value) }
-    end
+        settings.each_with_object({}) { |value, result| result.merge!(value) }
+      end
 
-    def self.content_types_for(from_settings)
-      if from_settings.present?
-        from_settings
-      else
-        Grape::ContentTypes::CONTENT_TYPES
+      def content_types_for(from_settings)
+        if from_settings.present?
+          from_settings
+        else
+          Grape::ContentTypes::CONTENT_TYPES.merge(default_elements)
+        end
       end
     end
   end
