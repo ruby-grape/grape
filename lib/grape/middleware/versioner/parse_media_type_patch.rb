1930c18bb62ec62c25f20b5c0ddc7411b5e99927
Eric Proulx <eproulx@petalmd.com>
2020-03-16 14:46:51 +0100

    Add SafeOperator before match

--- lib/grape/middleware/versioner/parse_media_type_patch.rb
+++ lib/grape/middleware/versioner/parse_media_type_patch.rb
@@ -8,7 +8,7 @@ module Rack
         # Corrected version of https://github.com/mjackson/rack-accept/blob/master/lib/rack/accept/header.rb#L40-L44
         def parse_media_type(media_type)
           # see http://tools.ietf.org/html/rfc6838#section-4.2 for allowed characters in media type names
-          m = media_type.match(ALLOWED_CHARACTERS)
+          m = media_type&.match(ALLOWED_CHARACTERS)
           m ? [m[1], m[2], m[3] || ''] : []
         end
       end
