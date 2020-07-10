ruby-grape/grape

commit 002280415a46b1cabea565533141298781a48553

Lazy lookup and lazy object (#2002)
* mime_types middleware cache.

* Replace %w by actual Supporter methods CONST

* Replace %w by Supported methods CONST

* Fixed [] leak. Its now generating empty array when needed.

* Introducing lazy generated lookup tables (cache) and lazy_object.

* Using Lazy_Object for request headers and Lazy lookup table when generating string. It reduced string allocations and lazy_object add a significant performance

* Added CHANGELOG.md entry.

* Change - to .freeze

* keys.include? replaced by key?(name)

lib/grape/router.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'grape/router/route'
+require 'grape/util/cache'
 
 module Grape
   class Router
@@ -16,12 +17,20 @@ module Grape
       end
     end
 
+    class NormalizePathCache < Grape::Util::Cache
+      def initialize
+        @cache ||= Hash.new do |h, path|
+          normalized_path = +"/#{path}"
+          normalized_path.squeeze!('/')
+          normalized_path.sub!(%r{/+\Z}, '')
+          normalized_path = '/' if normalized_path.empty?
+          h[path] = -normalized_path
+        end
+      end
+    end
+
     def self.normalize_path(path)
-      path = +"/#{path}"
-      path.squeeze!('/')
-      path.sub!(%r{/+\Z}, '')
-      path = '/' if path == ''
-      path
+      NormalizePathCache[path]
     end
 
     def self.supported_methods
@@ -160,7 +169,7 @@ module Grape
     end
 
     def call_with_allow_headers(env, methods, endpoint)
-      env[Grape::Env::GRAPE_ALLOWED_METHODS] = methods.join(', ')
+      env[Grape::Env::GRAPE_ALLOWED_METHODS] = methods.join(', ').freeze
       endpoint.call(env)
     end
 

lib/grape/api/instance.rb
@@ -74,7 +74,7 @@ module Grape
         # (see #cascade?)
         def cascade(value = nil)
           if value.nil?
-            inheritable_setting.namespace_inheritable.keys.include?(:cascade) ? !namespace_inheritable(:cascade).nil? : true
+            inheritable_setting.namespace_inheritable.key?(:cascade) ? !namespace_inheritable(:cascade).nil? : true
           else
             namespace_inheritable(:cascade, value)
           end
@@ -178,7 +178,7 @@ module Grape
       # errors from reaching upstream. This is effectivelly done by unsetting
       # X-Cascade. Default :cascade is true.
       def cascade?
-        return self.class.namespace_inheritable(:cascade) if self.class.inheritable_setting.namespace_inheritable.keys.include?(:cascade)
+        return self.class.namespace_inheritable(:cascade) if self.class.inheritable_setting.namespace_inheritable.key?(:cascade)
         return self.class.namespace_inheritable(:version_options)[:cascade] if self.class.namespace_inheritable(:version_options) && self.class.namespace_inheritable(:version_options).key?(:cascade)
         true
       end
@@ -209,7 +209,7 @@ module Grape
             route_settings[:endpoint] = route.app
 
             # using the :any shorthand produces [nil] for route methods, substitute all manually
-            route_settings[:methods] = %w[GET PUT POST DELETE PATCH HEAD OPTIONS] if route_settings[:methods].include?('*')
+            route_settings[:methods] = Grape::Http::Headers::SUPPORTED_METHODS if route_settings[:methods].include?('*')
           end
         end
 

lib/grape/dsl/inside_route.rb
@@ -177,7 +177,7 @@ module Grape
       def status(status = nil)
         case status
         when Symbol
-          raise ArgumentError, "Status code :#{status} is invalid." unless Rack::Utils::SYMBOL_TO_STATUS_CODE.keys.include?(status)
+          raise ArgumentError, "Status code :#{status} is invalid." unless Rack::Utils::SYMBOL_TO_STATUS_CODE.key?(status)
           @status = Rack::Utils.status_code(status)
         when Integer
           @status = status

lib/grape/dsl/routing.rb
@@ -142,11 +142,11 @@ module Grape
           reset_validations!
         end
 
-        %w[get post put head delete options patch].each do |meth|
-          define_method meth do |*args, &block|
+        Grape::Http::Headers::SUPPORTED_METHODS.each do |supported_method|
+          define_method supported_method.downcase do |*args, &block|
             options = args.extract_options!
             paths = args.first || ['/']
-            route(meth.upcase, paths, options, &block)
+            route(supported_method, paths, options, &block)
           end
         end
 

lib/grape/http/headers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'grape/util/lazy_object'
+
 module Grape
   module Http
     module Headers
@@ -26,6 +28,29 @@ module Grape
       HTTP_ACCEPT            = 'HTTP_ACCEPT'
 
       FORMAT                 = 'format'
+
+      HTTP_HEADERS = Grape::Util::LazyObject.new do
+        common_http_headers = %w[
+          Version
+          Host
+          Connection
+          Cache-Control
+          Dnt
+          Upgrade-Insecure-Requests
+          User-Agent
+          Sec-Fetch-Dest
+          Accept
+          Sec-Fetch-Site
+          Sec-Fetch-Mode
+          Sec-Fetch-User
+          Accept-Encoding
+          Accept-Language
+          Cookie
+        ].freeze
+        common_http_headers.each_with_object({}) do |header, response|
+          response["HTTP_#{header.upcase.tr('-', '_')}"] = header
+        end.freeze
+      end
 
       def self.find_supported_method(route_method)
         Grape::Http::Headers::SUPPORTED_METHODS.detect { |supported_method| supported_method.casecmp(route_method).zero? }

lib/grape/middleware/base.rb
@@ -74,11 +74,9 @@ module Grape
       end
 
       def mime_types
-        types_without_params = {}
-        content_types.each_pair do |k, v|
+        @mime_type ||= content_types.each_pair.with_object({}) do |(k, v), types_without_params|
           types_without_params[v.split(';').first] = k
         end
-        types_without_params
       end
 
       private

lib/grape/namespace.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'grape/util/cache'
+
 module Grape
   # A container for endpoints or other namespaces, which allows for both
   # logical grouping of endpoints as well as sharing common configuration.
@@ -25,13 +27,21 @@ module Grape
 
     # (see ::joined_space_path)
     def self.joined_space(settings)
-      (settings || []).map(&:space).join('/')
+      settings&.map(&:space)
     end
 
     # Join the namespaces from a list of settings to create a path prefix.
     # @param settings [Array] list of Grape::Util::InheritableSettings.
     def self.joined_space_path(settings)
-      Grape::Router.normalize_path(joined_space(settings))
+      Grape::Router.normalize_path(JoinedSpaceCache[joined_space(settings)])
+    end
+
+    class JoinedSpaceCache < Grape::Util::Cache
+      def initialize
+        @cache ||= Hash.new do |h, joined_space|
+          h[joined_space] = -joined_space.join('/')
+        end
+      end
     end
   end
 end

lib/grape/path.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'grape/util/cache'
+
 module Grape
   # Represents a path to an endpoint.
   class Path
@@ -58,7 +60,7 @@ module Grape
     end
 
     def path
-      Grape::Router.normalize_path(parts.join('/'))
+      Grape::Router.normalize_path(PartsCache[parts])
     end
 
     def path_with_suffix
@@ -70,6 +72,14 @@ module Grape
     end
 
     private
+
+    class PartsCache < Grape::Util::Cache
+      def initialize
+        @cache ||= Hash.new do |h, parts|
+          h[parts] = -parts.join('/')
+        end
+      end
+    end
 
     def parts
       parts = [mount_path, root_prefix].compact

commit 35d528f207a77be4c99c62fd3f5b231aaf6554e4

Fix memory leak caused by caching all unique paths seen
This optimisation was introduced in #2002. If an api's paths include
an id in the path then this cache can grow in an unbounded manner.

lib/grape/router.rb
@@ -7,20 +7,12 @@ module Grape
   class Router
     attr_reader :map, :compiled
 
-    class NormalizePathCache < Grape::Util::Cache
-      def initialize
-        @cache = Hash.new do |h, path|
-          normalized_path = +"/#{path}"
-          normalized_path.squeeze!('/')
-          normalized_path.sub!(%r{/+\Z}, '')
-          normalized_path = '/' if normalized_path.empty?
-          h[path] = -normalized_path
-        end
-      end
-    end
-
     def self.normalize_path(path)
-      NormalizePathCache[path]
+      path = +"/#{path}"
+      path.squeeze!('/')
+      path.sub!(%r{/+\Z}, '')
+      path = '/' if path == ''
+      path
     end
 
     def self.supported_methods