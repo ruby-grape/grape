d295f4656ea3cb6b281a0c5066927207594c0cca
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2021-11-23 13:42:45 -0600

    Expand test coverage of undocumented functionality
    In an interview recently, I was given the Grape repository sans the
    content of this method as a technical screening question. The task was
    to discover what had been removed from the project and re-implement the
    missing code using only the test suite.
    
    It was actually fairly difficult to re-implement this method because
    there are a couple of untested responsibilities. I think this is a
    fairly integral component of the framework, so it makes sense to fully
    test its functionality.

--- lib/grape/dsl/headers.rb
+++ lib/grape/dsl/headers.rb
@@ -3,8 +3,11 @@
 module Grape
   module DSL
     module Headers
-      # Set an individual header or retrieve
-      # all headers that have been set.
+      # This method has four responsibilities:
+      # 1. Set a specifc header value by key
+      # 2. Retrieve a specifc header value by key
+      # 3. Retrieve all headers that have been set
+      # 4. Delete a specifc header key-value pair
       def header(key = nil, val = nil)
         if key
           val ? header[key.to_s] = val : header.delete(key.to_s)
