9fee0a6fcf25994ee3b88fcc844978b7cb563d25
eproulx <eproulx@petalmd.com>
2021-12-25 18:26:31 +0100

    Add require 'active_support/isolated_execution_state' when ActiveSupport::VERSION::MAJOR > 6 Add rails-7

--- Appraisals
+++ Appraisals
@@ -12,6 +12,10 @@ appraise 'rails-6-1' do
   gem 'rails', '~> 6.1'
 end
 
+appraise 'rails-7' do
+  gem 'rails', '~> 7.0'
+end
+
 appraise 'rails-edge' do
   gem 'rails', github: 'rails/rails'
 end
--- lib/grape.rb
+++ lib/grape.rb
@@ -8,6 +8,11 @@ require 'rack/auth/basic'
 require 'rack/auth/digest/md5'
 require 'set'
 require 'active_support/version'
+
+if ActiveSupport::VERSION::MAJOR > 6
+  require 'active_support/isolated_execution_state'
+end
+
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/array/extract_options'
