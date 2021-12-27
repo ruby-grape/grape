890988210f64cb722bd8128b1ebe02035544826d
eproulx <eproulx@petalmd.com>
2021-12-27 21:30:10 +0100

    Put DryTypes back to Autoload

--- lib/grape.rb
+++ lib/grape.rb
@@ -45,6 +45,7 @@ module Grape
     autoload :Env, 'grape/util/env'
     autoload :Json, 'grape/util/json'
     autoload :Xml, 'grape/util/xml'
+    autoload :DryTypes
   end
 
   module Http
@@ -273,7 +274,6 @@ module Grape
   end
 end
 
-require 'grape/dry_types'
 require 'grape/config'
 require 'grape/content_types'
 
