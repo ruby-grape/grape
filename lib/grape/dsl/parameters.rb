19fe117a100a83ce858b37a84bc7947ea0d33c37
dm1try <me@dmitry.it>
2022-01-05 01:26:07 +0300

    support kwargs in shared params definition fixes #2231

--- lib/grape/dsl/parameters.rb
+++ lib/grape/dsl/parameters.rb
@@ -62,7 +62,7 @@ module Grape
           params_block = named_params.fetch(name) do
             raise "Params :#{name} not found!"
           end
-          instance_exec(options, &params_block)
+          instance_exec(**options, &params_block)
         end
       end
       alias use_scope use
