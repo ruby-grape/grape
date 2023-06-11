2b60c0c91cf2ff9f2c3efd805cf57268a60fc364
Eric Proulx <eproulx@petalmd.com>
2023-06-11 12:59:24 +0200

    Update Changelog's entry Update spec comment Add spec for anonymous class

--- spec/grape/validations/validators/base_spec.rb
+++ spec/grape/validations/validators/base_spec.rb
@@ -2,8 +2,18 @@
 
 RSpec.describe Grape::Validations::Validators::Base do
   describe '#inherited' do
-    # I tried with `Class.new(Grape::Validations::Validators::Base) but `inherited` is not called.
-    context "when class's name does not end with _validator" do
+    context "when validator is anonymous" do
+      subject(:custom_validator) { Class.new(described_class) }
+
+      it 'does not register the validator' do
+        expect(Grape::Validations).not_to receive(:register_validator)
+        custom_validator
+      end
+    end
+
+    # Anonymous class does not have a name and class A < B would leak.
+    # Simulates inherited callback
+    context "when validator's underscored name does not end with _validator" do
       subject(:custom_validator) { described_class.inherited(TestModule::CustomValidatorABC) }
 
       before { stub_const('TestModule::CustomValidatorABC', Class.new) }
@@ -14,12 +24,12 @@ RSpec.describe Grape::Validations::Validators::Base do
       end
     end
 
-    context "when class's name ends with _validator" do
+    context "when validator's underscored name ends with _validator" do
       subject(:custom_validator) { described_class.inherited(TestModule::CustomValidator) }
 
       before { stub_const('TestModule::CustomValidator', Class.new) }
 
-      it 'registers the custom validator with a short name not ending with validator' do
+      it 'registers the custom validator with short name not ending with validator' do
         expect(Grape::Validations).to receive(:register_validator).with('custom', TestModule::CustomValidator)
         custom_validator
       end
