2d87076bc693ad901bfdaee3a21bbbd0c25fb2b3
Stuart Chinery <stuart.chinery@boxt.co.uk>
2023-10-23 16:15:26 +0100

    Added other headers to integration specs

--- spec/integration/rack/v2/headers_spec.rb
+++ spec/integration/rack/v2/headers_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 describe Grape::Http::Headers do
+  it { expect(described_class::ALLOW).to eq('Allow') }
+  it { expect(described_class::CACHE_CONTROL).to eq('Cache-Control') }
+  it { expect(described_class::CONTENT_LENGTH).to eq('Content-Length') }
   it { expect(described_class::CONTENT_TYPE).to eq('Content-Type') }
+  it { expect(described_class::LOCATION).to eq('Location') }
+  it { expect(described_class::TRANSFER_ENCODING).to eq('Transfer-Encoding') }
   it { expect(described_class::X_CASCADE).to eq('X-Cascade') }
 end
