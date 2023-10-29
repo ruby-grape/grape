c67ec82099394ef42f542023ad6609f09ac5e1cf
Eric Proulx <eproulx@petalmd.com>
2023-10-29 12:01:39 +0100

    Update rubocop-todo

--- lib/grape/validations.rb
+++ lib/grape/validations.rb
@@ -3,7 +3,7 @@
 module Grape
   module Validations
     def self.require_validator(short_name)
-     Grape::Validations::Validators.const_get("#{short_name.to_s.camelize}Validator")
+      Grape::Validations::Validators.const_get("#{short_name.to_s.camelize}Validator")
     rescue NameError
       raise Grape::Exceptions::UnknownValidator, short_name
     end
