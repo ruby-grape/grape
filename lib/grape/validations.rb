95dbfa9802f665ff99e4f233d5a1ee52cdf98359
Eric Proulx <eproulx@petalmd.com>
2023-10-29 11:58:40 +0100

    Remove validator register/unregister Remove Coercer collection

--- lib/grape/validations.rb
+++ lib/grape/validations.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 module Grape
-  # Registry to store and locate known Validators.
   module Validations
-    module_function
-
-    # Find a validator and if not found will try to load it
-    def require_validator(short_name)
-      Grape::Validations::Validators.const_get("#{short_name.to_s.camelize}Validator")
+    def self.require_validator(short_name)
+     Grape::Validations::Validators.const_get("#{short_name.to_s.camelize}Validator")
     rescue NameError
-      raise Grape::Exceptions::UnknownValidator.new(short_name)
+      raise Grape::Exceptions::UnknownValidator, short_name
     end
   end
 end
