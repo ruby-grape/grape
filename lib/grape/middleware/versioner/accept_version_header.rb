229248601f2e1dde205b1671ccc29d154183c16f
Eric Proulx <eproulx@petalmd.com>
2024-07-27 23:39:09 +0300

    Revisit versioner middlewares (#2484)
    * AcceptHeaderHandler is now part of Grape::Middleware::Versioner::Header
    Use `const_get` to find versioner
    Grape::Middleware::Versioner::* uses `default_options` like other middlewares
    Add versioner_helpers for Grape::Middleware::Versioner::*
    Replace `merge` by `deep_merge` in Grape::Middleware::Base initialize
    Add specs
    
    * Add CHANGELOG entry
    
    * Remove prefix throw_ and add!
    Use `camelize` instead of `classify`

--- lib/grape/middleware/versioner/accept_version_header.rb
+++ lib/grape/middleware/versioner/accept_version_header.rb
@@ -17,45 +17,22 @@ module Grape
       # X-Cascade header to alert Grape::Router to attempt the next matched
       # route.
       class AcceptVersionHeader < Base
+        include VersionerHelpers
+
         def before
-          potential_version = (env[Grape::Http::Headers::HTTP_ACCEPT_VERSION] || '').strip
+          potential_version = env[Grape::Http::Headers::HTTP_ACCEPT_VERSION]&.strip
+          not_acceptable!('Accept-Version header must be set.') if strict? && potential_version.blank?
 
-          if strict? && potential_version.empty?
-            # If no Accept-Version header:
-            throw :error, status: 406, headers: error_headers, message: 'Accept-Version header must be set.'
-          end
+          return if potential_version.blank?
 
-          return if potential_version.empty?
-
-          # If the requested version is not supported:
-          throw :error, status: 406, headers: error_headers, message: 'The requested version is not supported.' unless versions.any? { |v| v.to_s == potential_version }
-
+          not_acceptable!('The requested version is not supported.') unless potential_version_match?(potential_version)
           env[Grape::Env::API_VERSION] = potential_version
         end
 
         private
 
-        def versions
-          options[:versions] || []
-        end
-
-        def strict?
-          options[:version_options] && options[:version_options][:strict]
-        end
-
-        # By default those errors contain an `X-Cascade` header set to `pass`, which allows nesting and stacking
-        # of routes (see Grape::Router) for more information). To prevent
-        # this behavior, and not add the `X-Cascade` header, one can set the `:cascade` option to `false`.
-        def cascade?
-          if options[:version_options]&.key?(:cascade)
-            options[:version_options][:cascade]
-          else
-            true
-          end
-        end
-
-        def error_headers
-          cascade? ? { Grape::Http::Headers::X_CASCADE => 'pass' } : {}
+        def not_acceptable!(message)
+          throw :error, status: 406, headers: error_headers, message: message
         end
       end
     end
--- lib/grape/middleware/versioner/header.rb
+++ lib/grape/middleware/versioner/header.rb
@@ -22,17 +22,10 @@ module Grape
       # X-Cascade header to alert Grape::Router to attempt the next matched
       # route.
       class Header < Base
-        def before
-          handler = Grape::Util::AcceptHeaderHandler.new(
-            accept_header: env[Grape::Http::Headers::HTTP_ACCEPT],
-            versions: options[:versions],
-            **options.fetch(:version_options) { {} }
-          )
+        include VersionerHelpers
 
-          handler.match_best_quality_media_type!(
-            content_types: content_types,
-            allowed_methods: env[Grape::Env::GRAPE_ALLOWED_METHODS]
-          ) do |media_type|
+        def before
+          match_best_quality_media_type! do |media_type|
             env.update(
               Grape::Env::API_TYPE => media_type.type,
               Grape::Env::API_SUBTYPE => media_type.subtype,
@@ -40,6 +33,98 @@ module Grape
               Grape::Env::API_VERSION => media_type.version,
               Grape::Env::API_FORMAT => media_type.format
             )
+          end
+        end
+
+        private
+
+        def match_best_quality_media_type!
+          return unless vendor
+
+          strict_header_checks!
+          media_type = Grape::Util::MediaType.best_quality(accept_header, available_media_types)
+          if media_type
+            yield media_type
+          else
+            fail!(allowed_methods)
+          end
+        end
+
+        def allowed_methods
+          env[Grape::Env::GRAPE_ALLOWED_METHODS]
+        end
+
+        def accept_header
+          env[Grape::Http::Headers::HTTP_ACCEPT]
+        end
+
+        def strict_header_checks!
+          return unless strict?
+
+          accept_header_check!
+          version_and_vendor_check!
+        end
+
+        def accept_header_check!
+          return if accept_header.present?
+
+          invalid_accept_header!('Accept header must be set.')
+        end
+
+        def version_and_vendor_check!
+          return if versions.blank? || version_and_vendor?
+
+          invalid_accept_header!('API vendor or version not found.')
+        end
+
+        def q_values_mime_types
+          @q_values_mime_types ||= Rack::Utils.q_values(accept_header).map(&:first)
+        end
+
+        def version_and_vendor?
+          q_values_mime_types.any? { |mime_type| Grape::Util::MediaType.match?(mime_type) }
+        end
+
+        def invalid_accept_header!(message)
+          raise Grape::Exceptions::InvalidAcceptHeader.new(message, error_headers)
+        end
+
+        def invalid_version_header!(message)
+          raise Grape::Exceptions::InvalidVersionHeader.new(message, error_headers)
+        end
+
+        def fail!(grape_allowed_methods)
+          return grape_allowed_methods if grape_allowed_methods.present?
+
+          media_types = q_values_mime_types.map { |mime_type| Grape::Util::MediaType.parse(mime_type) }
+          vendor_not_found!(media_types) || version_not_found!(media_types)
+        end
+
+        def vendor_not_found!(media_types)
+          return unless media_types.all? { |media_type| media_type&.vendor && media_type.vendor != vendor }
+
+          invalid_accept_header!('API vendor not found.')
+        end
+
+        def version_not_found!(media_types)
+          return unless media_types.all? { |media_type| media_type&.version && versions&.exclude?(media_type.version) }
+
+          invalid_version_header!('API version not found.')
+        end
+
+        def available_media_types
+          [].tap do |available_media_types|
+            base_media_type = "application/vnd.#{vendor}"
+            content_types.each_key do |extension|
+              versions&.reverse_each do |version|
+                available_media_types << "#{base_media_type}-#{version}+#{extension}"
+                available_media_types << "#{base_media_type}-#{version}"
+              end
+              available_media_types << "#{base_media_type}+#{extension}"
+            end
+
+            available_media_types << base_media_type
+            available_media_types.concat(content_types.values.flatten)
           end
         end
       end
