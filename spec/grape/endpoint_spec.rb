1067f37430281fa7d9d08f42386783f92b3f43a2
Eric <eproulx@petalmd.com>
2024-09-01 19:36:28 +0200

    Change backtick for single quote in test

--- spec/grape/endpoint_spec.rb
+++ spec/grape/endpoint_spec.rb
@@ -683,7 +683,7 @@ describe Grape::Endpoint do
     context 'when referencing an undefined local variable or method' do
       let(:error_message) do
         if Gem::Version.new(RUBY_VERSION).release <= Gem::Version.new('3.2')
-          %r{undefined local variable or method `undefined_helper' for #<Class:0x[0-9a-fA-F]+> in `/hey' endpoint}
+          %r{undefined local variable or method `undefined_helper' for #<Class:0x[0-9a-fA-F]+> in '/hey' endpoint}
         else
           /undefined local variable or method `undefined_helper' for/
         end
