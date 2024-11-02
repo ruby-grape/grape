d4406747f9bf4d04724f535e4e56b8a0cb5f2a6e
Eric Proulx <eproulx@petalmd.com>
2024-11-02 14:28:19 +0100

    Revert compile_many_routes.rb

--- benchmark/compile_many_routes.rb
+++ benchmark/compile_many_routes.rb
@@ -3,23 +3,20 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'grape'
 require 'benchmark/ips'
-require 'memory_profiler'
 
 class API < Grape::API
   prefix :api
   version 'v1', using: :path
 
-  namespace :test do
-    2000.times do |index|
-      namespace index do
-        get do
-          'hello'
-        end
-      end
+  2000.times do |index|
+    get "/test#{index}/" do
+      'hello'
     end
   end
 end
 
-MemoryProfiler.report(allow_files: 'grape') do
-  API.compile!
-end.pretty_print(to_file: 'without_double_splat.txt')
+Benchmark.ips do |ips|
+  ips.report('Compiling 2000 routes') do
+    API.compile!
+  end
+end
