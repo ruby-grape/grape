f3a735aa420a2e1e759fa04f81668126f9a9bc03
Eric Proulx <eproulx@petalmd.com>
2024-12-15 15:55:33 +0100

    Add Grape::Formatter::Base and use Grape::Util::Registry

--- lib/grape/formatter/json.rb
+++ lib/grape/formatter/json.rb
@@ -2,13 +2,11 @@
 
 module Grape
   module Formatter
-    module Json
-      class << self
-        def call(object, _env)
-          return object.to_json if object.respond_to?(:to_json)
+    class Json < Base
+      def self.call(object, _env)
+        return object.to_json if object.respond_to?(:to_json)
 
-          ::Grape::Json.dump(object)
-        end
+        ::Grape::Json.dump(object)
       end
     end
   end
