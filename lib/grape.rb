b4d4719cf1c4b5dcc993a9a1078f29a8811a6825
Andrey Subbota <subbota@gmail.com>
2025-12-07 02:46:27 +0100

    Add comment for tests

--- lib/grape.rb
+++ lib/grape.rb
@@ -77,6 +77,8 @@ module Grape
   end
 end
 
+# Comment for testing purposes
+
 # https://api.rubyonrails.org/classes/ActiveSupport/Deprecation.html
 # adding Grape.deprecator to Rails App if any
 require 'grape/railtie' if defined?(Rails::Railtie) && ActiveSupport.gem_version >= Gem::Version.new('7.1')
