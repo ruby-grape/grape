fdbbfe59de60315f37acc248495ea7a620b04177
Michael N <michael.nikitochkin@gmx.net>
2026-02-21 19:35:40 +0100

    Skip JSON formatting if the object is already a string

--- lib/grape/formatter/json.rb
+++ lib/grape/formatter/json.rb
@@ -4,6 +4,7 @@ module Grape
   module Formatter
     class Json < Base
       def self.call(object, _env)
+        return object if object.is_a?(String)
         return object.to_json if object.respond_to?(:to_json)
 
         ::Grape::Json.dump(object)
